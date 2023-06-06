2b11d02707779a9c04be594b30ffa32e570b9633
akarsh <akarsh91140@gmail.com>
2023-06-07 01:48:09 +0530

    aks

--- php/get_stats_projects.php
+++ php/get_stats_projects.php
@@ -1,16 +1,18 @@
-<?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/php/database_get_data.php");
-$arr = array();
+
 $arr = json_decode(json_encode(get_all_user_projects($_GET["id"])));
-$retrn = new stdClass();
-if ($arr != null)
-    for ($i = 0; $i < count($arr); $i++) {
-        $ret[$i] = json_decode(get_project_by_id($arr[$i]->project_id));
-        $id = $ret[$i]->project_name;
-        $retrn->$id++;
+$retrn = array();
+
+if ($arr != null) {
+for ($i = 0; $i < count($arr); $i++) { $ret=json_decode(get_project_by_id($arr[$i]->project_id));
+    $id = $ret->project_name;
+
+    if (!isset($retrn[$id])) {
+    $retrn[$id] = 1; // Initialize count to 1 if project name not found in $retrn
+    } else {
+    $retrn[$id]++; // Increment count if project name already exists in $retrn
+    }
+    }
     }
 
-
-
-
-echo json_encode($retrn);
+    echo json_encode($retrn);
