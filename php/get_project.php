a8f742b3f27b22cf09f4f408b28743ce9f5f46f4
akarsh <akarsh91140@gmail.com>
2023-06-07 15:58:23 +0530

    user access to functions fixed

--- php/get_project.php
+++ php/get_project.php
@@ -2,5 +2,6 @@
 require_once($_SERVER['DOCUMENT_ROOT'] . "/php/database_get_data.php");
 $arr = json_decode(get_project_by_id($_GET["id"]));
 $id = $arr->team_leader;
+$arr->team_leader_id = $id;
 $arr->team_leader = get_user_name($id);
 echo json_encode($arr);
