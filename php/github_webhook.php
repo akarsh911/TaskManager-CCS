33ad114e37b48792bce4aafc3a2dda365cee5f29
Akarsh <akarsh91140@gmail.com>
2023-06-08 12:05:19 +0530

    Resolved Commit History

--- php/github_webhook.php
+++ php/github_webhook.php
@@ -1,20 +1,18 @@
 <?php
 require_once($_SERVER['DOCUMENT_ROOT'] . "/php/database_set_data.php");
 require_once($_SERVER['DOCUMENT_ROOT'] . "/php/database_get_data.php");
-$response = fetch_response($_GET["repo"]);
+//$response = fetch_response($_GET["repo"]);
 $request_body = file_get_contents('php://input');
-echo $request_body;
-$data = json_decode($response);
-
-if (does_commit_exist($data[0]->sha) == false) {
-    echo json_encode($data[0]) . "<br>";
-    echo json_encode($data[0]->author) . "<br>";
-    $user = json_decode(get_user_by_github($data[0]->author->login));
+//echo $request_body;
+$data = json_decode($request_body);
+$commits=$data->commits;
+if (does_commit_exist($commits[0]->id) == false) {
+    $user = json_decode(get_user_by_github($data->sender->login));
     $project = json_decode(get_project_by_repo_name($_GET["repo"]));
     $user_id = $user->id;
     $repo_id = $project->id;
-    $msg = sanitizeText($data[0]->commit->message);
-    create_user_contribution($user_id, $repo_id, $data[0]->sha, $data[0]->author->login, $msg);
+    $msg = sanitizeText($commits[0]->message);
+    create_user_contribution($user_id, $repo_id, $commits[0]->id, $data->sender->login, $msg);
     echo "Success";
 } else {
     echo "Failed Commit already exists";
@@ -24,34 +22,4 @@ function sanitizeText($text)
     $text = str_replace("'", "", $text);
     $text = str_replace('"', '', $text);
     return $text;
-}
-
-function fetch_response($repo)
-{
-
-    $owner = 'ccs-tiet-task';
-    $ch = curl_init('https://api.github.com/repos/' . $owner . '/' . $repo . '/commits');
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_HTTPGET, true);
-    //curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-    curl_setopt(
-        $ch,
-        CURLOPT_HTTPHEADER,
-        array(
-            'Accept: application/vnd.github+json',
-            'X-GitHub-Api-Version: 2022-11-28',
-            'Content-Type: application/json',
-            'user-agent: github'
-        )
-    );
-    curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-    $response = curl_exec($ch);
-    curl_close($ch);
-
-    if (json_decode($response)[0]->sha != "") {
-        return $response;
-    } else {
-        echo "Failed to create webhook.";
-        echo $response;
-    }
 }
